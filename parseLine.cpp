a374c4ee4eff1b6f7341f849f6f1e96e19bac784
KotaAoyama <hongcha811@gmail.com>
2020-02-25 16:32:41 +0900

    store the board using the state enum

--- parseLine.cpp
+++ parseLine.cpp
@@ -11,35 +11,45 @@ using std::istringstream;
 
 enum class State{kEmpty, kObstacle};
 
-vector<int> ParseLine(string line){
+vector<State> ParseLine(string line){
   istringstream sline(line);
   int n;
   char c;
-  vector<int> row;
+  // TODO: Change the variable type for row
+  // to be a vector<State>
+  vector<State> row;
   while (sline >> n >> c && c == ',') {
-    row.push_back(n);
+    if (n == 0) {
+      row.push_back(State::kEmpty);
+    } else {
+      row.push_back(State::kObstacle);
+    }
   }
   return row;
 }
 
-
-vector<vector<int> > ReadBoardFile(string path) {
+// TODO: Modify the return type here as well. Just
+// as above, the board will contain State objects
+// instead of ints.
+vector<vector<State> > ReadBoardFile(string path) {
   ifstream myfile (path);
-  vector< vector<int> >board{};
+  // TODO: Modify the board declarationto store 
+  // State objects instead of ints.
+  vector< vector<State> >board{};
   if (myfile) {
     string line;
     while (getline(myfile, line)) {
-      vector<int> row = ParseLine(line);
+      // TODO: Modify the row type to use State
+      // objects instead of ints.
+      vector<State> row = ParseLine(line);
       board.push_back(row);
     }
   }
   return board;
 }
 
-// TODO: Create the CellString function here,
-// using the following return strings:
-// "⛰️   "
-// "0   "
+// TODO: Modify the function signature below to accept
+// cells with State type instead of int type.
 string CellString(State cell) {
   if (cell == State::kObstacle) {
     return "⛰️   ";
@@ -48,10 +58,12 @@ string CellString(State cell) {
   }
 }
 
-void PrintBoard(const vector<vector<int> > board) {
+void PrintBoard(const vector<vector<State> > board) {
   for (int i = 0; i < board.size(); i++) {
     for (int j = 0; j < board[i].size(); j++) {
-      cout << board[i][j];
+      // TODO: Modify the line below to call CellString
+      // on each element of the board before streaming to cout.
+      cout << CellString(board[i][j]);
     }
     cout << "\n";
   }
