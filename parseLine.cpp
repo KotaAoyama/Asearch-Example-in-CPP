e0c9f18d386c22ece0c23773a2ac9b753eb5bea5
KotaAoyama <hongcha811@gmail.com>
2020-02-25 16:36:22 +0900

    removed comment

--- parseLine.cpp
+++ parseLine.cpp
@@ -11,12 +11,11 @@ using std::istringstream;
 
 enum class State{kEmpty, kObstacle};
 
+
 vector<State> ParseLine(string line){
   istringstream sline(line);
   int n;
   char c;
-  // TODO: Change the variable type for row
-  // to be a vector<State>
   vector<State> row;
   while (sline >> n >> c && c == ',') {
     if (n == 0) {
@@ -28,19 +27,13 @@ vector<State> ParseLine(string line){
   return row;
 }
 
-// TODO: Modify the return type here as well. Just
-// as above, the board will contain State objects
-// instead of ints.
+
 vector<vector<State> > ReadBoardFile(string path) {
   ifstream myfile (path);
-  // TODO: Modify the board declarationto store 
-  // State objects instead of ints.
   vector< vector<State> >board{};
   if (myfile) {
     string line;
     while (getline(myfile, line)) {
-      // TODO: Modify the row type to use State
-      // objects instead of ints.
       vector<State> row = ParseLine(line);
       board.push_back(row);
     }
@@ -48,8 +41,7 @@ vector<vector<State> > ReadBoardFile(string path) {
   return board;
 }
 
-// TODO: Modify the function signature below to accept
-// cells with State type instead of int type.
+
 string CellString(State cell) {
   if (cell == State::kObstacle) {
     return "⛰️   ";
@@ -58,16 +50,16 @@ string CellString(State cell) {
   }
 }
 
+
 void PrintBoard(const vector<vector<State> > board) {
   for (int i = 0; i < board.size(); i++) {
     for (int j = 0; j < board[i].size(); j++) {
-      // TODO: Modify the line below to call CellString
-      // on each element of the board before streaming to cout.
       cout << CellString(board[i][j]);
     }
     cout << "\n";
   }
 }
+
 
 int main() {
   auto board = ReadBoardFile("./data/1.board");
